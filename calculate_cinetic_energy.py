ab576bdf7f5f99c4ab8a14456de1a0418a486fef
davidmerfu <davidmerfu23@gmail.com>
2025-09-30 12:11:01 +0200

    Update calculate_cinetic_energy.py

--- calculate_cinetic_energy.py
+++ calculate_cinetic_energy.py
@@ -2,4 +2,7 @@
 masa=float(input("Introduce la masa"))
 velocidad=float(input("Introduce la velocidad"))
 cinetic_energy= 0.5 * masa * velocidad**2
-print(cinetic_energy)
+mensaje= f"La energia cinetica es {cinetic_energy} julios"
+print(mensaje)
+
+      
